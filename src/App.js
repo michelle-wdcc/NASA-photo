4d8545201c06cbd03ed661b9dc745a02ae0f5df9
michelle-wdcc <69370899+michelle-wdcc@users.noreply.github.com>
2021-08-19 17:26:53 +1200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/Home.js";
+import NasaPhoto from "./components/NasaPhoto";
+import "./App.css";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Route component={Home} path="/" exact />
+        <Route component={NasaPhoto} path="/nasaphoto" />
+      </div>
+    </BrowserRouter>
+  );
+}
+/*
 function App() {
   return (
     <div className="App">
@@ -22,4 +36,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
